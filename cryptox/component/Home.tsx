4fb2697fd88864b8a1167f6194e73ef8ce46f7aa
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-04-23 00:32:03 +0530

    css added and useState

--- cryptox/component/Home.tsx
+++ cryptox/component/Home.tsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+const [amount, setAmount] = useState('');
+const [receiver, setReceiver] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>CryptoX Wallet</Text>
+            <TextInput style={styles.input} placeholder='Enter Amount'  />
+            <TextInput style={styles.input} placeholder='Enter receiver id' />
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Send</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff'
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        marginBottom: 10,
+        width: '80%',
+        borderRadius: 5
+    },
+    button: {
+        backgroundColor: '#007AFF',
+        padding: 10,
+        borderRadius: 5
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+});
