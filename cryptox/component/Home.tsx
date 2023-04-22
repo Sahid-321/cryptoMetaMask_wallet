f70c645049ac2a64ce0ff73bd06b04f5c4134f4c
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-04-23 04:16:02 +0530

    loader added

--- cryptox/component/Home.tsx
+++ cryptox/component/Home.tsx
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator, Alert } from 'react-native';
 import axios from 'axios';
 
 export default function Home() {
     const [amount, setAmount] = useState<string>('');
     const [receiver, setReceiver] = useState<string>('');
     const [apiData, setApiData] = useState([])
-
+    const [loader, setLoader] = useState(false);
     const objToPassInApi = {
         amount,
         receiver
     }
+    useEffect(() => {
+        if (apiData) {
+            setLoader(false)
+        }
+    }, [apiData])
 
-    const handleSend = () => {
-        //const acc2 = "0x64040DD45739eb952B8003eF87D9C1d4F347De48";
-        axios.post<{ data: any }>('http://192.168.1.72:3000/balances', objToPassInApi)
-            .then(({ data }) => setApiData(data))
-            .catch((err) => console.log(err));
+    const handleSend = async () => {
+        if (amount == '' || receiver == '') {
+            Alert.alert("Please fill input field")
+        } else {
+            setLoader(true);
+            await axios.post<{ data: any }>('http://192.168.1.72:3000/balances', objToPassInApi)
+                .then(({ data }) => setApiData(data))
+                .catch((err) => console.log(err));
+
+
+        }
+
+
     };
 
     return (
@@ -25,10 +38,20 @@ export default function Home() {
             <TextInput style={styles.input} placeholder='Enter Amount less than 0.0025' onChangeText={(text) => setAmount(text)} />
             <TextInput style={styles.input} placeholder='Enter receiver id' onChangeText={(text) => setReceiver(text)} />
 
-            <Text>sender Balance Before: {apiData.senderBalanceBefore}</Text>
-            <Text>receiver Balance Before: {apiData.receiverBalanceeBefor}</Text>
-            <Text>sender Balance After: {apiData.senderBalanceAfter}</Text>
-            <Text>receiver Balance After: {apiData.receiverBalanceAfter}</Text>
+
+            {loader ? <View>
+                <ActivityIndicator size="large" color="#00ff00" />
+                <Text>Wait 30-40seconds while the transaction complete</Text>
+            </View> :
+                <View>
+                    <Text>sender Balance Before: {apiData.senderBalanceBefore}</Text>
+                    <Text>receiver Balance Before: {apiData.receiverBalanceeBefor}</Text>
+                    <Text>sender Balance After: {apiData.senderBalanceAfter}</Text>
+                    <Text>receiver Balance After: {apiData.receiverBalanceAfter}</Text>
+
+                </View>}
+
+
 
             <TouchableOpacity style={styles.button} onPress={handleSend}>
                 <Text style={styles.buttonText}>Send</Text>
