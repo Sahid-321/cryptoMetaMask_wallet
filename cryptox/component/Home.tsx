e06b74ab8b8147d07e57e7f2934e0d52e08de6b6
SahidJamal-1 <75063419+Sahid-321@users.noreply.github.com>
2023-04-24 13:45:15 +0530

    changed api to direct in native

--- cryptox/component/Home.tsx
+++ cryptox/component/Home.tsx
@@ -1,38 +1,77 @@
+import "@ethersproject/shims";
+import { ethers } from "ethers";
 import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator, Alert, Dimensions } from 'react-native';
-
 const { width, height } = Dimensions.get('window');
 const screenWidth = width < height ? width : height;
-
-import axios from 'axios';
 
 export default function Home() {
     const [amount, setAmount] = useState<string>('');
     const [receiver, setReceiver] = useState<string>('');
-    const [apiData, setApiData] = useState([])
+    const [senderBefore, setSenderBefore] = useState<any>('')
+    const [senderAfter, setSenderAfter] = useState<any>('')
+    const [receiverBefore, setReceiverBefore] = useState<any>('')
+    const [receiverAfter, setReceiverAfter] = useState<any>('')
+    const [gasPriceDetail, setGasPriceDetail] = useState<any>('')
+
     const [loader, setLoader] = useState(false);
-    const objToPassInApi = {
-        amount,
-        receiver
-    }
+
     useEffect(() => {
-        if (apiData) {
+        if (senderAfter) {
             setLoader(false)
         }
-    }, [apiData])
+    }, [senderAfter])
 
     const handleSend = async () => {
-        if (amount == '' || receiver == '') {
-            Alert.alert("Please fill input field")
-        } else {
+          console.log("clicked");
+        try {
+            const provider = await new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/fc531493ede94c64bab557ac727c9361`);
+
+            const account1 = '0x64040DD45739eb952B8003eF87D9C1d4F347De48'; // Your account address 1
+            const account2 = receiver; // Your account address 2
+
+            const privateKey1 = '967bcdb3e507ec56e41a0e289a0582fb88e0466fcf89f388bb94147487e802e9'; // Private key of account 1
+            const wallet = new ethers.Wallet(privateKey1, provider);
+
+            const main = async () => {
+                const senderBalanceBefore = await provider.getBalance(account1);
+                const receiverBalanceBefore = await provider.getBalance(account2);
+
+                setSenderBefore(ethers.utils.formatEther(senderBalanceBefore));
+                setReceiverBefore(ethers.utils.formatEther(receiverBalanceBefore));
+
+                console.log(`\nSender balance before: ${ethers.utils.formatEther(senderBalanceBefore)}`);
+                console.log(`Receiver balance before: ${ethers.utils.formatEther(receiverBalanceBefore)}\n`);
+
+                const gasPrice = ethers.utils.parseUnits("50", "gwei");
+                const tx = await wallet.sendTransaction({
+                    to: account2,
+                    value: ethers.utils.parseEther(amount),
+                    gasPrice: gasPrice,
+                });
+
+                await tx.wait();
+                console.log(`Transaction hash: ${tx.hash}`);
+                console.log(`Gas used: ${tx.gasLimit.toString()}`);
+                console.log(`Gas price: ${ethers.utils.formatUnits(gasPrice, "gwei")}`);
+
+                const senderBalanceAfter = await provider.getBalance(account1);
+                const receiverBalanceAfter = await provider.getBalance(account2);
+
+                setSenderAfter(ethers.utils.formatEther(senderBalanceAfter));
+                setReceiverAfter(ethers.utils.formatEther(receiverBalanceAfter));
+                setGasPriceDetail(ethers.utils.formatUnits(gasPrice, "gwei"));
+
+                console.log(`\nSender balance after: ${ethers.utils.formatEther(senderBalanceAfter)}`);
+                console.log(`Receiver balance after: ${ethers.utils.formatEther(receiverBalanceAfter)}\n`);
+            };
+
+            main();
             setLoader(true);
-            await axios.post<{ data: any }>('http://192.168.1.72:3000/balances', objToPassInApi)
-                .then(({ data }) => setApiData(data))
-                .catch((err) => console.log(err));
 
-
+        } catch (error) {
+            console.log(error, "error")
         }
-
 
     };
 
@@ -48,12 +87,12 @@ export default function Home() {
                 <Text>Wait 30-40seconds while the transaction complete</Text>
             </View> :
                 <View>
-                    <Text><Text style={[styles.text, { color:"orange"}]}>Sender Balance Before: </Text>{apiData.senderBalanceBefore}</Text>
-                    <Text><Text style={[styles.text, { color:"orange"}]}>Receiver Balance Before: </Text>{apiData.receiverBalanceeBefor}</Text>
+                    <Text><Text style={[styles.text, { color: "orange" }]}>Sender Balance Before: </Text>{senderBefore}</Text>
+                    <Text><Text style={[styles.text, { color: "orange" }]}>Receiver Balance Before: </Text>{receiverBefore}</Text>
                     <Text style={{ color: "green" }}>---------------------------------------</Text>
-                    <Text><Text style={[styles.text, { color:"green"}]}>Sender Balance After: </Text>{apiData.senderBalanceAfter}</Text>
-                    <Text><Text style={[styles.text, { color:"green"}]}>Receiver Balance After: </Text>{apiData.receiverBalanceAfter}</Text>
-                    <Text><Text style={[styles.text, { color:"orange"}]}>GasPrice: </Text>{apiData.gasPrice}</Text>
+                    <Text><Text style={[styles.text, { color: "green" }]}>Sender Balance After: </Text>{senderAfter}</Text>
+                    <Text><Text style={[styles.text, { color: "green" }]}>Receiver Balance After: </Text>{receiverAfter}</Text>
+                    <Text><Text style={[styles.text, { color: "orange" }]}>GasPrice: </Text>{gasPriceDetail} gwei</Text>
 
                 </View>}
 
